56b512437a20e2d70f9da745888d5ab27c65be73
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-06-05 00:41:10 -0500

    Adicionado o cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express, { json } from 'express';
+import cors from 'cors';
 import routes from './routes';
 import path from 'path'
-const app = express();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
