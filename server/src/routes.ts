d76781c00159c7a4666c83cfe61df0845b33c135
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-06-05 00:35:33 -0500

    Criado as rotas do backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,23 @@
 import express from 'express';
+import knex from './database/connection';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 
 const routes = express.Router();
 
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 
-routes.get('/users', (req, res)=>{
+routes.get('/items', itemsController.index);
 
-    return res.send({message: 'Hello World'})
-});
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
 
 
 
