eec3876f672dd4f98fca823f8c1a19954df34bca
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-06-07 16:34:58 -0500

    Criado as paginas de navegaçao

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -3,8 +3,15 @@ import { Feather as Icon } from '@expo/vector-icons';
 import {View, ImageBackground, Text, Image, Button} from 'react-native';
 import styles from './style';
 import {RectButton} from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home (){
+    const navigation = useNavigation();
+function handleNavigatorToPoints(){
+    navigation.navigate('Points'); 
+}
+
+
     return(
 <ImageBackground style={styles.container} 
 source={require('../../assets/home-background.png')}
@@ -17,7 +24,7 @@ imageStyle={{width:274, height:368}}
     <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coletas de forma eficiente.</Text>
 </View>
         <View style={styles.footer}>
-       <RectButton style={styles.button} onPress={()=>{}}>
+       <RectButton style={styles.button} onPress={handleNavigatorToPoints}>
            <View style={styles.buttonIcon}>
                <Text>
                    <Icon name="arrow-right" color="#FFF" size={24}/>
