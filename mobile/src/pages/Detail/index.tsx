f9810bd9a1e629193b0e3e8c680e57fd4b7764fe
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-06-07 21:42:09 -0500

    Adicionado a API na pagina de Detail

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
-import {View, TouchableOpacity, Text, Image, SafeAreaView} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {View, TouchableOpacity, Text, Image, SafeAreaView, Linking} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import {Feather as Icon, FontAwesome} from '@expo/vector-icons';
 import {RectButton} from 'react-native-gesture-handler';
+
+import * as MailComposer from 'expo-mail-composer';
+
+
+
 import styles from './style';
 
 
+
+import api from '../../services/api';
+
+interface Params{
+    point_id: string;
+}
+interface Point{
+    point:{
+    id: number;
+    image: string;
+    name: string;
+    email:string;
+    whatsapp:string;
+    city: string;
+    uf: string;
+    },
+    items:[{
+        title: string;
+    }]
+   
+ }
+
 const Detail = ()=>{
+    const [PointData, setPointData] = useState<Point>({} as Point);
+    
+    
+    
+    const route = useRoute();
+    const routeParams = route.params as Params;
+
+    useEffect(()=>{
+        api.get(`points/${routeParams.point_id}`).then(res=>{
+     setPointData(res.data)
+    })
+    },[])
+
+    if(!PointData.point){
+        //Para o fututo será retornado um Loading
+        return null;
+    }
+
     const navigation = useNavigation();
     
     
      function handleNavigatorBack(){
         navigation.goBack();
     }   
+
+    function handleComposeMail(){
+        MailComposer.composeAsync({
+            subject:'Interesse na coleta de resíduos',
+            recipients: [PointData.point.email],
+        });
+    }
+    function handleWhatsapp(){
+     Linking.openURL(`whatsapp://send?phone=${PointData.point.whatsapp}&text="Tenho interesse sobre coleta de resíduos"`)
+    }
+
+
      return (
          <SafeAreaView style={{flex: 1}}>
         <View style={styles.container}>
@@ -20,23 +77,27 @@ const Detail = ()=>{
         <Icon name="arrow-left" size={20} color="#34cb49" />
         </TouchableOpacity>  
 
-        <Image style={styles.pointImage} source={{uri: 'https://unsplash.com/photos/N-MqWXXZvNY/download?force=true&w=640'}}/>
-        <Text style={styles.pointName}>Mercado do seu Zé</Text>
-        <Text style={styles.pointItems}>Lampadas, Óleo de Cozinha</Text>
+        <Image style={styles.pointImage} source={{uri: PointData.point.image}}/>
+        <Text style={styles.pointName}>{PointData.point.name}</Text>
+        
+        <Text style={styles.pointItems}>
+            Lampadas, Óleo de Cozinha
+            {PointData.items.map(item=>item.title).join(', ')}
+            </Text>
         
         <View style={styles.address}>
          <Text style={styles.addressTitle}>Endereço</Text>
-         <Text style={styles.addressContent}>Caraguatatuba, SP</Text>
+     <Text style={styles.addressContent}>{PointData.point.city}, {PointData.point.uf}</Text>
         </View>
         </View>
         <View style={styles.footer}>
 
-            <RectButton style={styles.button} onPress={()=>{}}>
+            <RectButton style={styles.button} onPress={handleWhatsapp}>
             <FontAwesome name="whatsapp" size={20} color="#FFF"/>
             <Text style={styles.buttonText}>Whatsapp</Text>
             </RectButton>
 
-            <RectButton style={styles.button} onPress={()=>{}}>
+            <RectButton style={styles.button} onPress={handleComposeMail}>
             <Icon name="mail" size={20} color="#FFF"/>
             <Text style={styles.buttonText}>E-Mail</Text>
             </RectButton>
